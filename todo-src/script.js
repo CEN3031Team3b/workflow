f717f4f39f100d32dd75aa534fdf299f777418e3
Noah@Noah <noahpresser@ufl.edu>
2015-09-14 22:14:45 -0400

    I did it, kinda roundabout but works
    My summary is a description

--- todo-src/script.js
+++ todo-src/script.js
@@ -3,15 +3,34 @@
 var myApp = angular.module('app', []);
 
 myApp.controller('MainCtrl', function ($scope){
-  $scope.todos = ["Learn Angular", "Learn node"];
+   var defineString = function (firstString, secondString) {
+    var string = firstString + " Priority: " + secondString;
+    return string;
+  }  
+  var redefineString = function ( originalString, secondString) {
+    var endIndex = originalString.search(" Priority");
+    var string = originalString.substring(0, endIndex);
+    string = string + " Priority: " + secondString;
+    return string
+  }
+
+  $scope.todos = [defineString("try", "fail")];
   $scope.newItem = "";
-  
+
+
   $scope.addItem = function(){
     console.log("in add");
     if ($scope.newItem !== ""){
-      $scope.todos.push($scope.newItem);
+      var newItem = defineString($scope.newItem, "fail");
+      $scope.todos.push(newItem);
       $scope.newItem = "";
     }
+  }
+  $scope.changePriority = function(item){
+    console.log("in changePriority");
+    var priority = prompt("What is the priority of this event?");
+    var index = $scope.todos.indexOf(item);
+    $scope.todos[index] = redefineString($scope.todos[index], priority);
   }
     
   $scope.deleteItem = function(item){
